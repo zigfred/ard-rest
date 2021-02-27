cb6ce3d786dcda515c22a4ac4fc75913cca9ab25
zigfred <zigfredead@gmail.com>
2021-02-27 02:46:52 +0200

    REST - extended heartbeat with power outage status

--- api/controllers/heartbeatController.js
+++ api/controllers/heartbeatController.js
@@ -1,5 +1,13 @@
+var mongoose = require('mongoose'),
+  Collector = mongoose.model('Collector');
 
 exports.get = function(req, res) {
-  res.end();
+  Collector.findOne()
+  .select('powerCheck')
+  .sort('-_id')
+  .exec( function(err, data) {
+    if (err)
+      res.send(err);
+    res.json(data);
+  });
 };
-
